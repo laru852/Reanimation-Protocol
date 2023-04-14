7eed72225d7376d393e58380bcdc43a9f9e13af8
Layne Rupar <laru852@student.oaisd.org>
2023-04-14 10:33:38 -0400

    4/14/23
    Added Goblin and new enemy movement and attack system

---
+++ Reanimation Protocol/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    #region Public Variables
+    public Transform rayCast;
+    public LayerMask raycastMask;
+    public float rayCastLength;
+    public float attackDistance; 
+    public float moveSpeed;
+    public float timer;
+    #endregion
+
+    #region Private Variables
+    private RaycastHit2D hit;
+    private GameObject target;
+    private Animator anim;
+    private float distance; 
+    private bool attackMode;
+    private bool inRange;
+    private bool cooling;
+    private float intTimer;
+    #endregion
+    
+    void Awake()
+    {
+        intTimer = timer;
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inRange)
+        {
+            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, raycastMask);
+            RaycastDebugger();
+        }
+
+        //When player is detected
+        if(hit.collider != null)
+        {
+            EnemyLogic();
+        }
+        else if(hit.collider == null)
+        {
+            inRange = false;
+        }
+
+        if(inRange == false)
+        {
+            anim.SetBool("canWalk", false);
+            StopAttack();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D trig) 
+    {
+        if(trig.gameObject.tag == "Player")
+        {
+            target = trig.gameObject;
+            inRange = true;
+        }
+    }
+
+    void EnemyLogic()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+
+        if(distance > attackDistance)
+        {
+            Move();
+            StopAttack();
+        }
+        else if(attackDistance >= distance && cooling == false)
+        {
+            Attack();
+        }
+
+        if (cooling)
+        {
+            Cooldown();
+            anim.SetBool("Attack", false);
+        }
+    }
+
+    void Move()
+    {
+        anim.SetBool("canWalk", true);
+        if(!anim.GetCurrentAnimatorStateInfo(0).IsName("G_attack"))
+        {
+            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void Attack()
+    {
+        timer = intTimer; //Reset timer when player enter attack range 
+        attackMode = true; //To check if Enemy can still attack or not
+
+        anim.SetBool("canWalk", false);
+        anim.SetBool("Attack", true);
+    }
+
+    void Cooldown()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0 && cooling && attackMode)
+        {
+            //cooling = false;
+            timer = intTimer;
+        }
+    }
+
+    void StopAttack()
+    {
+        cooling = false;
+        attackMode = false;
+        anim.SetBool("Attack", false);
+    }
+    
+    void RaycastDebugger()
+    {
+        if(distance > attackDistance)
+        {
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
+        }
+        else if(attackDistance > distance)
+        {
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.green);
+        }
+    }
+
+    public void TriggerCooling()
+    {
+        cooling = true;
+    }
+}
---
+++ Reanimation Protocol/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealth = 100;
+    int currentHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        animator.SetTrigger("Hurt");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+        
+    }
+
+    void Die()
+    {
+        animator.SetBool("Isdead", true);
+        this.enabled = false;
+    }
+}
